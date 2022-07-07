05c55d057817d8cadb2614486bd43d759618af4f
jlambert12013 <jlambert12013@gmail.com>
2022-07-07 16:12:55 -0500

    BEST VERSION

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,10 +1,32 @@
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
+import Link from 'next/link'
 
-const Article = () => {
-  const router = useRouter()
-  const { id } = router.query
+const Article = ({ article }) => {
+  //   This is an Example using NEXT JS
+  //   const router = useRouter()
+  //   const { id } = router.query
 
-  return <div>This is article {id}</div>
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <Link href="/">Go Back</Link>
+    </>
+  )
+}
+
+//  Get Server-side Props
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `http://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  )
+
+  const article = await res.json()
+  return {
+    props: {
+      article,
+    },
+  }
 }
 
 export default Article
