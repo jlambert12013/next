a2179bdef34fe6b64eca234ac6689ca892754dbd
jlambert12013 <jlambert12013@gmail.com>
2022-07-07 14:37:18 -0500

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head' // For HTML Head Tags
+// import styles from '../styles/Layout.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Alabama Powersports | 2925 Citizens Pkwy, Selma AL 36701</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="description" content="" />
+      </Head>
+      <h1>Welcome to Alabama Powersports</h1>
+    </div>
+  )
+}
